1229fec3615a10725ef964cd345f44b6b948d3f2
philip yang <philipyangly12@gmail.com>
2020-02-09 18:55:45 +1100

    Updated

--- src/main/java/arrays/MedianOfTwoSortedArrays.java
+++ src/main/java/arrays/MedianOfTwoSortedArrays.java
@@ -1,7 +1,41 @@
 package arrays;
 
+/**
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/
+ */
 public class MedianOfTwoSortedArrays {
-    public double find(int[] nums1, int[] nums) {
-        return 1.0;
+    // returns leftmost (or rightmost) index at which `target` should be
+    // inserted in sorted array `nums` via binary search.
+    private int extremeInsertionIndex(int[] nums, int target, boolean isLeft) {
+        int low = 0;
+        int high = nums.length;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target || (isLeft && target == nums[mid])) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] targetRange = {-1, -1};
+
+        int leftIdx = extremeInsertionIndex(nums, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if (leftIdx == nums.length || nums[leftIdx] != target) {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = extremeInsertionIndex(nums, target, false) - 1;
+
+        return targetRange;
     }
 }
--- src/test/java/arrays/MedianOfTwoSortedArraysTest.java
+++ src/test/java/arrays/MedianOfTwoSortedArraysTest.java
@@ -2,12 +2,25 @@ package arrays;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MedianOfTwoSortedArraysTest {
     @Test
     void whenValidate_thenOk() {
         MedianOfTwoSortedArrays test = new MedianOfTwoSortedArrays();
-        assertNotNull(test);
+        int[] nums = new int[]{5, 7, 7, 8, 8, 8, 10};
+        int target = 8;
+        int[] actual = test.searchRange(nums, target);
+        int[] expect = new int[]{3, 5};
+        assertEquals(Arrays.toString(expect), Arrays.toString(actual));
+    }
+
+    @Test
+    void whenTest_thenOk() {
+        int mid = 7 / 2;
+        System.out.println(mid);
+        assertEquals(3, mid);
     }
 }
