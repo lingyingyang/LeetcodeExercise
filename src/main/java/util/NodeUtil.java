285a3d3abbff30989e7b3f3dcdf0ae044ef4ae96
philip yang <philipyangly12@gmail.com>
2020-02-14 22:28:54 +1100

    Added MergeSortedLists

---
+++ src/main/java/util/NodeUtil.java
@@ -0,0 +1,14 @@
+package util;
+
+import tree.ListNode;
+
+public class NodeUtil {
+    public static String print(ListNode root) {
+        StringBuffer out = new StringBuffer();
+        while (root != null) {
+            out.append(String.format("%d -> ", root.val));
+            root = root.next;
+        }
+        return out.toString();
+    }
+}
